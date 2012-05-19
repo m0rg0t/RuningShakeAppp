04737544b7349a3d8f897983914b8cb4020dd56a
Anton Lenev <m0rg0t.Anton@gmail.com>
2012-05-19 13:58:39 +0400

    ShakeLib test

--- RuningApp/MainPage.xaml.cs
+++ RuningApp/MainPage.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
+using ShakeGestures;
+//using Microsoft.Phone.Controls;
+//using ShakeGestures;
 
 namespace RuningApp
 {
@@ -34,5 +37,40 @@ namespace RuningApp
                 App.ViewModel.LoadData();
             }
         }
+
+        public ShakeType CurrentShakeType
+        {
+            get { return (ShakeType)GetValue(CurrentShakeTypeProperty); }
+            set { SetValue(CurrentShakeTypeProperty, value); }
+        }
+
+        public static readonly DependencyProperty CurrentShakeTypeProperty =
+            DependencyProperty.Register("CurrentShakeType", typeof(ShakeType), typeof(MainPage), new PropertyMetadata(ShakeType.X));
+
+        private void RecordGestures_Click(object sender, RoutedEventArgs e)
+        {
+            ShakeGesturesHelper.Instance.ShakeGesture += new EventHandler<ShakeGestureEventArgs>(Instance_ShakeGesture);
+
+            // optional, set parameters
+            ShakeGesturesHelper.Instance.MinimumRequiredMovesForShake = 1;
+            ShakeGesturesHelper.Instance.WeakMagnitudeWithoutGravitationThreshold = 0.05;
+
+            // start shake detection
+            ShakeGesturesHelper.Instance.Active = true;
+        }
+
+        private void Instance_ShakeGesture(object sender, ShakeGestureEventArgs e)
+        {
+            Dispatcher.BeginInvoke(
+                () =>
+                {
+                    //Text = DateTime.Now.ToString();
+                    CurrentShakeType = e.ShakeType;
+                    //MessageBox.Show(e.ShakeType.ToString());
+                    this.ShakeLog.Items.Add(DateTime.Now.Minute + ":" + DateTime.Now.Second + ":" + DateTime.Now.Millisecond + "  " + e.ShakeType);
+                });
+            //System.Diagnostics.Debug.WriteLine(DateTime.Now.Minute + ":" + DateTime.Now.Second + ":" + DateTime.Now.Millisecond + "  " + e.ShakeType);
+        }
+
     }
 }
